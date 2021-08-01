6e9595557c72964ff424f69b119d91af67886a03
ShengGuangSheng <shengguangsheng@baimaohui.net>
2021-08-01 18:10:01 +0800

    init typeorm

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+
+createConnection().then(async connection => {
+
+    console.log(connection);
+    await connection.close()
+
+}).catch(error => console.log(error));
