279367b4a95c33e474c0f5f4eedccada971870c3
ShengGuangSheng <shengguangsheng@baimaohui.net>
2021-08-05 00:32:36 +0800

    获取 connection 方法优化，解决无法获取 metadata 的bug，继续优化

--- lib/getDBConnection.tsx
+++ lib/getDBConnection.tsx
@@ -2,35 +2,25 @@ import {createConnection, getConnectionManager} from 'typeorm';
 import {User} from 'src/entity/User';
 import {Post} from 'src/entity/Post';
 import {Discussion} from 'src/entity/Discussion';
-import 'reflect-metadata'
-import ormconfig from 'ormconfig.json'
+import 'reflect-metadata';
+import ormconfig from 'ormconfig.json';
 
 // 解决 js 无法识别 metadata 的问题
-const create = async ()=>{
+const create = async () => {
     // @ts-ignore
     return createConnection({
         ...ormconfig,
-        entities: [User,Post,Discussion]
+        entities: [User, Post, Discussion]
     });
-}
+};
 
 // 使用立即执行函数，解决js中的await不能在顶层的问题
 const promise = (async function () {
     const manager = getConnectionManager();
-    // 如果不存在连接，则创建新的
-    if (!manager.has('default')) {
-        return create();
-    } else {
-        const currentConnection = manager.get('default');
-        // 如果存在连接，且还在连接状态，则使用旧的
-        if (currentConnection.isConnected) {
-            return currentConnection;
-        }
-        // 如果存在连接，且已经断开了，则创建新的
-        else {
-            return create();
-        }
-    }
+    const currentConnection = manager.has('default') && manager.get('default');
+    // 如果存在连接，则先关闭。这是为了解决开发环境每次修改代码，如果使用旧的连接，则依然无法识别到metadata的bug。
+    if (currentConnection) await currentConnection.close();
+    return create();
 })();
 
 export const getDBConnection = async () => {
