f5df32f01e3c207630dfff1eb982211184b501bb
ShengGuangSheng <shengguangsheng@baimaohui.net>
2021-08-29 16:11:16 +0800

    引入 ant design of react

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'styles/globals.scss'
 import 'styles/posts.scss'
 import 'styles/sign_up.scss'
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
