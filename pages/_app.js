252e05a626d23a44a5963a995f24ddb72d268f69
ShengGuangSheng <shengguangsheng@baimaohui.net>
2021-08-30 11:39:12 +0800

    redux 命名规范，调整为 store

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import 'styles/sign_up.scss'
 import 'antd/dist/antd.css';
 import 'styles/iconfont.css'
 import {Provider} from 'react-redux'
-import {useStore} from 'redux/store'
+import {useStore} from 'store/store'
 
 const App = ({Component, pageProps}) => {
     const store = useStore(pageProps.initialReduxState)
